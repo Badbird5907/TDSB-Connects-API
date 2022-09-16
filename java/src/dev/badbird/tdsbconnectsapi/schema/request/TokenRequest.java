d20abdeae6f8d8f8275204e0b9d7d4e15184b963
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-09-16 09:05:26 -0400

    Added refresh token support

--- java/src/dev/badbird/tdsbconnectsapi/schema/request/TokenRequest.java
+++ java/src/dev/badbird/tdsbconnectsapi/schema/request/TokenRequest.java
@@ -2,14 +2,27 @@ package dev.badbird.tdsbconnectsapi.schema.request;
 
 import dev.badbird.tdsbconnectsapi.TDSBConnects;
 import dev.badbird.tdsbconnectsapi.schema.response.TokenResponse;
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
 import okhttp3.*;
 
-@AllArgsConstructor
 @Getter
 @Setter
 public class TokenRequest implements APIRequest<TokenResponse> {
     private String username, password;
+
+    private String refreshToken = null;
+
+    public TokenRequest(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public TokenRequest(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
     @Override
     public String getEndpoint() {
         return "/token";
@@ -25,12 +38,16 @@ public class TokenRequest implements APIRequest<TokenResponse> {
 
     @Override
     public Request.Builder addData(Request.Builder builder) {
-        RequestBody formBody = new FormBody.Builder()
-                .add("username", username)
-                .add("password", password)
-                .add("grant_type", "password")
-                .build();
+        FormBody.Builder formBody = new FormBody.Builder();
+        if (refreshToken == null) {
+            formBody.add("grant_type", "password");
+            formBody.add("username", username);
+            formBody.add("password", password);
+        } else {
+            formBody.add("grant_type", "refresh_token");
+            formBody.add("refresh_token", refreshToken);
+        }
 
-        return builder.post(formBody);
+        return builder.post(formBody.build());
     }
 }
