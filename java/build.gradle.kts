647bec4848e07853aa5bd4d7621f121b78e289c3
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-12-07 15:34:07 -0500

    Maven central publishing

--- java/build.gradle.kts
+++ java/build.gradle.kts
@@ -1,9 +1,13 @@
 plugins {
     id("java")
+    id("maven-publish")
+    id("signing")
+    //id("com.github.johnrengelman.shadow") version "7.1.2" // TODO - make shadowjar work
 }
 
-group = "dev.dev.badbird"
-version = "1.0-SNAPSHOT"
+group = "dev.badbird"
+version = "0.0.1-DEV"
+description = "TDSB Connects API Wrapper"
 
 repositories {
     mavenCentral()
@@ -31,3 +35,81 @@ sourceSets {
         java.srcDir("src/main/test")
     }
 }
+val nexusUsername: String by project
+val nexusPassword: String by project
+val sourcesJar = tasks.register("sourcesJar", Jar::class.java) {
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+val javadocJar = tasks.register("javadocJar", Jar::class.java) {
+    classifier = "javadoc"
+    from(sourceSets.main.get().allJava)
+}
+
+apply(plugin = "java")
+apply(plugin = "maven-publish")
+apply(plugin = "signing")
+
+publishing {
+    repositories {
+        maven {
+            mavenLocal()
+            val releasesRepoUrl = "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2"
+            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+            //url = version.endsWith('SNAPSHOT') ? snapshotsRepoUrl : releasesRepoUrl
+            setUrl(releasesRepoUrl)
+            credentials {
+                username = nexusUsername
+                password = nexusPassword
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("mavenJava") {
+            pom {
+                name.set("TDSBConnectsAPI")
+                description.set("TDSB Connects API Wrapper")
+                url.set("https://github.com/Badbird5907/TDSB-Connects-API")
+                from(components["java"])
+                artifact(sourcesJar)
+                artifact(javadocJar)
+                scm {
+                    url.set("https://github.com/Badbird5907/TDSB-Connects-API.git")
+                }
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT/")
+                        distribution.set("repo")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("Badbird5907")
+                        name.set("Badbird5907")
+                        email.set("contact@badbird.dev")
+                    }
+                }
+            }
+        }
+    }
+}
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+tasks {
+    artifacts {
+        archives(javadocJar)
+        archives(sourcesJar)
+    }
+    jar {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        from({
+            configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+        })
+    }
+}
+//tasks.getByName("jar").dependsOn(tasks.getByName("shadowJar"))
+tasks.getByName("test").onlyIf { tasks.getByName("test").hasProperty("test") }
