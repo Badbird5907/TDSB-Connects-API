2422e5a05b2ebe671f2a5378c63c3475675aa02b
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-12-09 18:31:35 -0500

    add another date format

--- java/src/main/java/dev/badbird/tdsbconnectsapi/util/Utilities.java
+++ java/src/main/java/dev/badbird/tdsbconnectsapi/util/Utilities.java
@@ -6,19 +6,27 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Utilities {
-    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+    private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"),
+    otherDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
     public static long parseDateToUnixMillis(String in) { // Thanks chatgpt
         // the date format that the string follows
 
         try {
             // parse the date string using the specified format
-            Date date = dateFormat.parse(in);
+            Date date;
+
+            if (in.contains("Z")) {
+                date = dateFormat.parse(in);
+            } else {
+                date = otherDateFormat.parse(in);
+            }
 
             // convert the Date object to a unix timestamp (the number of milliseconds since January 1, 1970)
 
             // return the unix timestamp
             return date.getTime();
         } catch (ParseException e) {
+            e.printStackTrace();
             // if the date string could not be parsed, return 0
             return -1;
         }
--- java/src/main/test/dev/badbird/tdsbconnectsapi/util/UtilitiesTest.java
+++ java/src/main/test/dev/badbird/tdsbconnectsapi/util/UtilitiesTest.java
@@ -10,6 +10,9 @@ public class UtilitiesTest {
         String in = "2022-12-15T20:10:30.012Z";
         long out = Utilities.parseDateToUnixMillis(in);
         System.out.println("Out: " + out);
+        String in1 = "2022-12-15T14:00:00";
+        long out1 = Utilities.parseDateToUnixMillis(in1);
+        System.out.println("Out1: " + out1);
         assertEquals(1671153030012L, out);
         System.out.println("Today: " + Utilities.getToday());
     }
